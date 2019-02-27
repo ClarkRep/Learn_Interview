b206c45ef0fd1a2c7ec18ce45766811ed6ec24cc
clark <806843367@qq.com>
2019-02-27 16:18:31 +0800

    移除无用文件

--- app/src/main/java/com/clark/learn/interview/MainActivity.kt
+++ app/src/main/java/com/clark/learn/interview/MainActivity.kt
@@ -1,7 +1,6 @@
-gipackage com.clark.learn.interview
-
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.clark.learn.interview.R
 
 class MainActivity : AppCompatActivity() {
 
